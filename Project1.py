ab5e36e941392a5a64bf9746c05f5b9118597433
Akash Sarkar <98306221+Astro-maestro@users.noreply.github.com>
2022-07-10 16:33:57 +0530

    Create Project1.py

---
+++ Project1.py
@@ -0,0 +1,26 @@
+# Author name - Akash Sarkar
+# College Finder Data Science Internship Project 1
+# Student Portfolio
+# Date - 10.07.22
+
+print("Enter the student's Name,Std,Division,Roll No., Marks and percentage of any three subjects :")
+name = str(input("Please enter the name of the student :"))
+std = int(input("Please enter the standard of the student :"))
+div =str(input("Please enter the division of the student :"))
+roll = int(input("Please enter the roll number of the student :"))
+engmarks = int(input("Please enter english subject marks of the student"))
+mathmarks = int(input("Please enter mathematics subject marks of the student"))
+scimarks = int(input("Please enter science subject marks of the student"))
+percentengmarks = int(input("Please enter percentage marks of english subject"))
+percentmathmarks = int(input("Please enter percentage marks of mathematics subject"))
+percentscimarks = int(input("Please enter percentage marks of science subject"))
+print("The name of the student is :",name)
+print("The standard of the student is :",std)
+print("The division of the student is :",div)
+print("The roll number of the student is :",roll)
+print("The english marks of the student is :",engmarks)
+print("The mathematics marks of the student is :",mathmarks)
+print("The science marks of the student is :",scimarks)
+print("The english marks percentage of the student is :" ,percentengmarks, "Percent.")
+print("The mathematics marks percentage of the student is :" ,percentmathmarks,"Percent.")
+print("The science marks percentage of the student is :" ,percentscimarks,"Percent.")
