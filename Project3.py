80478341db2c7a27c3127f481c902f6cea0f1e2e
Akash Sarkar <98306221+Astro-maestro@users.noreply.github.com>
2022-07-11 11:35:29 +0530

    Create Project3.py

---
+++ Project3.py
@@ -0,0 +1,10 @@
+# Author Name -Akash Sarkar
+# College Finder Internship Project 2 
+# Multiplication Table  
+# Date - 11.07.22 
+print(" Simple Tables:")
+a = int(input("Enter the number of your choice:\n"))
+i = 1
+while (i<12):
+    print(a,"*",i,"=",i*a)
+    i = i + 1
